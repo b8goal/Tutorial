52325cb8a4379faa6e7828d424b82cb6e31c625e
b8goal <30991265+b8goal@users.noreply.github.com>
2019-07-04 17:35:16 +0900

    Feature/practice (#4)
    * add reference object access
    
    * how to know related shared_ptrs
    
    * add temporary data & swap ref_data to tmp_data
    
    * add compare new_data to ref_data
    
    * add func3 for dynamic arry of shared_ptr
    
    * add func4 for shared_ptr's trap
    
    * change the variable name
    
    change the variable name

--- test.cpp
+++ test.cpp
@@ -22,6 +22,26 @@ using namespace std::chrono;
  * 
  */
 
+class Particle
+{
+public:
+  Particle()
+  {
+    std::cout << "Particle creates. " << std::endl;
+  }
+  Particle(int ID)
+  {
+    ID_ = ID;
+    std::cout << "Particle " << ID_ << " creates. " << std::endl;
+  }
+  ~Particle()
+  {
+    std::cout << "Particle " << ID_ << " deletes." << std::endl;
+  }
+  int ID_;
+  std::shared_ptr<Particle> circular_;
+};
+
 struct compare
 {
 	int key;
@@ -100,9 +120,11 @@ void func1(const std::shared_ptr<std::string> data)
   std::cout << "Here is in func1." << std::endl;
   std::cout << "data's address: " << data << std::endl;
   std::cout << "data's value:" << data->c_str() << std::endl;
+
+  *data = "func1: data1";
 }
 
-void func2(std::shared_ptr<std::string>& data)
+void func2(std::shared_ptr<std::string> &data)
 {
   std::cout << "Here is in func2." << std::endl;
   std::cout << "data's address: " << data << std::endl;
@@ -111,7 +133,24 @@ void func2(std::shared_ptr<std::string>& data)
   *data = "func2: data2";
 }
 
-int main(int argc, char* argv[])
+void func3(std::shared_ptr<Particle> data)
+{
+  std::cout << "shared_ptr - use dynamic array" << std::endl;
+  data.get()[0].ID_ = 88;
+  data.get()[1].ID_ = 99;
+  std::cout << data.get()[0].ID_ << ", " << data.get()[1].ID_ << std::endl;
+}
+
+void func4(std::shared_ptr<Particle> data1, std::shared_ptr<Particle> data2)
+{
+  data1->circular_ = data2;
+  data2->circular_ = data1;
+
+  std::cout << "data1's ID: " << data1->circular_ << std::endl;
+  std::cout << "data2's ID: " << data2->circular_ << std::endl;
+}
+
+int main(int argc, char *argv[])
 {
   //  Create new object in the heap memory
   std::shared_ptr<std::string> new_data = std::make_shared<std::string>("Hello, world!");
@@ -119,25 +158,60 @@ int main(int argc, char* argv[])
 
   std::cout << "new_data's address: " << new_data << std::endl;
   std::cout << "ref_data's address: " << ref_data << std::endl;
-  
-  std::cout << "new_data's count: " << new_data.use_count()<<std::endl;
-  std::cout << "ref_data's count: " << ref_data.use_count()<<std::endl;
+
+  std::cout << "new_data's count: " << new_data.use_count() << std::endl;
+  std::cout << "ref_data's count: " << ref_data.use_count() << std::endl;
 
   func1(ref_data);
   func2(new_data);
 
-  std::cout << "new_data's value:" << *new_data << std::endl;
-  std::cout << "ref_data's value:" << *ref_data << std::endl;
+  std::shared_ptr<std::string> tmp_data = std::make_shared<std::string>("Temporary Data");
+
+  std::cout << "*****************************" << std::endl;
+  std::cout << "new_data's value: " << *new_data << std::endl;
+  std::cout << "ref_data's value: " << *ref_data << std::endl;
+  std::cout << "tmp_data's value: " << *tmp_data << std::endl;
+
+  std::cout << "new_data's address: " << new_data.get() << std::endl;
+  std::cout << "ref_data's address: " << ref_data << std::endl;
+  std::cout << "tmp_data's address " << tmp_data << std::endl;
+
+  ref_data.swap(tmp_data);
+
+  std::cout << "new_data's value: " << *new_data << std::endl;
+  std::cout << "ref_data's value: " << *ref_data << std::endl;
+  std::cout << "tmp_data's value: " << *tmp_data << std::endl;
+
+  // Unrelated shared_ptrs are never equal.
+  std::cout << "new_data > ref_data = " << std::boolalpha << (new_data > ref_data) << std::endl;
+  std::cout << "new_data < ref_data = " << std::boolalpha << (new_data < ref_data) << std::endl;
+  std::cout << "new_data == ref_data = " << std::boolalpha << (new_data == ref_data) << std::endl;
+  std::cout << "*****************************" << std::endl;
 
   new_data.reset();
   ref_data.reset();
 
-  std::cout << "new_data's count: " << new_data.use_count()<<std::endl;
-  std::cout << "ref_data's count: " << ref_data.use_count()<<std::endl;
+  std::cout << "new_data's count: " << new_data.use_count() << std::endl;
+  std::cout << "ref_data's count: " << ref_data.use_count() << std::endl;
+
+  algorithm_test();
 
   algorithm_test();
 
   //  I don't care about how to free the new_data memory that was allocated heap memory using the malloc function.
+
+  std::shared_ptr<Particle> ParticlePtr(new Particle[2], std::default_delete<Particle[]>());
+  func3(ParticlePtr);
+
+  // Even if the program is terminated, the object created by the circular reference is not destroyed
+  
+  std::cout<<std::endl;
+  std::shared_ptr<Particle> ParticlePtr1(new Particle(1));
+  {
+    std::shared_ptr<Particle> ParticlePtr2(new Particle(2));
+    func4(ParticlePtr1, ParticlePtr2);
+  }
+  std::cout << "data1's ID: " << ParticlePtr1->ID_ << std::endl;
 
   return 0;
 }
