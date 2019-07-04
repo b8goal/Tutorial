875a07b01c9803905ba5147c19d7eb50f6101a0e
hyeonseong <b8goal@naver.com>
2019-07-04 17:04:02 +0900

    change the variable name
    change the variable name

--- test.cpp
+++ test.cpp
@@ -11,6 +11,11 @@
 #include <iostream>
 #include <memory>
 #include <string>
+#include <vector>
+#include <algorithm>
+#include <chrono>
+
+using namespace std::chrono;
 
 /**
  * @brief How to build this source: g++ test.cpp -std=c++11 -o test
@@ -24,18 +29,91 @@ public:
   {
     std::cout << "Particle creates. " << std::endl;
   }
-  Particle(int nID)
+  Particle(int ID)
   {
-    m_nID = nID;
-    std::cout << "Particle " << m_nID << " creates. " << std::endl;
+    ID_ = ID;
+    std::cout << "Particle " << ID_ << " creates. " << std::endl;
   }
   ~Particle()
   {
-    std::cout << "Particle " << m_nID << " deletes." << std::endl;
+    std::cout << "Particle " << ID_ << " deletes." << std::endl;
   }
-  int m_nID;
-  std::shared_ptr<Particle> m_Other;
+  int ID_;
+  std::shared_ptr<Particle> circular_;
 };
+
+struct compare
+{
+	int key;
+	compare(int const &i): key(i) { }
+
+	bool operator()(int const &i)
+	{
+		return (i == key);
+	}
+};
+
+void show_message(const std::string& title, bool found, high_resolution_clock::time_point t1, high_resolution_clock::time_point t2)
+{
+  std::cout << title << std::endl;
+
+	if (found)
+		std::cout << "Element found" << std::endl;
+	else
+		std::cout << "Element not found" << std::endl;
+
+  std::cout << "Duration: " << duration_cast<microseconds>( t2 - t1 ).count() << std::endl;
+  std::cout << "----------------------" << std::endl;
+}
+
+void algorithm_test()
+{
+	std::vector<int> v = { 4, 7, 5, 2, 6, 9 };
+	int key = rand() % 4;
+	
+	for (int i = 0; i < 9000; i++)
+		v.push_back(rand() % 3000);
+	
+	high_resolution_clock::time_point t1;
+	
+	bool found = false;
+	t1 = high_resolution_clock::now();
+		
+	for (auto it : v) {
+		if (it == key) {
+			found = true;
+			break;
+		}
+  }
+
+  show_message("Don't use the algorithm...", found, t1, high_resolution_clock::now());
+	
+	found = false;
+	t1 = high_resolution_clock::now();
+	
+	if (std::find(v.begin(), v.end(), key) != v.end())
+		found = true;
+
+	show_message("Use std::find", found, t1, high_resolution_clock::now());
+	
+	found = false;
+	t1 = high_resolution_clock::now();
+	
+	if (!std::none_of(v.begin(), v.end(), compare(key)))
+		found = true;
+
+	show_message("Use std::none_of", found, t1, high_resolution_clock::now());
+
+	std::sort(v.begin(), v.end());
+	
+	found = false;
+	t1 = high_resolution_clock::now();
+	
+	if (std::binary_search(v.begin(), v.end(), key))
+		found = true;
+
+	show_message("Use std::binary_search", found, t1, high_resolution_clock::now());
+}
 
 void func1(const std::shared_ptr<std::string> data)
 {
@@ -58,18 +136,18 @@ void func2(std::shared_ptr<std::string> &data)
 void func3(std::shared_ptr<Particle> data)
 {
   std::cout << "shared_ptr - use dynamic array" << std::endl;
-  data.get()[0].m_nID = 88;
-  data.get()[1].m_nID = 99;
-  std::cout << data.get()[0].m_nID << ", " << data.get()[1].m_nID << std::endl;
+  data.get()[0].ID_ = 88;
+  data.get()[1].ID_ = 99;
+  std::cout << data.get()[0].ID_ << ", " << data.get()[1].ID_ << std::endl;
 }
 
 void func4(std::shared_ptr<Particle> data1, std::shared_ptr<Particle> data2)
 {
-  data1->m_Other = data2;
-  data2->m_Other = data1;
+  data1->circular_ = data2;
+  data2->circular_ = data1;
 
-  std::cout << "data1's ID: " << data1->m_nID << std::endl;
-  std::cout << "data2's ID: " << data2->m_nID << std::endl;
+  std::cout << "data1's ID: " << data1->circular_ << std::endl;
+  std::cout << "data2's ID: " << data2->circular_ << std::endl;
 }
 
 int main(int argc, char *argv[])
@@ -116,6 +194,8 @@ int main(int argc, char *argv[])
   std::cout << "new_data's count: " << new_data.use_count() << std::endl;
   std::cout << "ref_data's count: " << ref_data.use_count() << std::endl;
 
+  algorithm_test();
+
   //  I don't care about how to free the new_data memory that was allocated heap memory using the malloc function.
 
   std::shared_ptr<Particle> ParticlePtr(new Particle[2], std::default_delete<Particle[]>());
@@ -129,7 +209,7 @@ int main(int argc, char *argv[])
     std::shared_ptr<Particle> ParticlePtr2(new Particle(2));
     func4(ParticlePtr1, ParticlePtr2);
   }
-  std::cout << "data1's ID: " << ParticlePtr1->m_nID << std::endl;
+  std::cout << "data1's ID: " << ParticlePtr1->ID_ << std::endl;
 
   return 0;
 }
