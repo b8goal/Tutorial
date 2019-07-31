441502af1529fcd6032a534676b6514afaa77e02
b8goal <b8goal@naver.com>
2019-07-31 17:35:25 +0900

    CMake and googletest

---
+++ googletest/test3/src/ClassA.cpp
@@ -0,0 +1,8 @@
+#include "ClassA.h"
+
+int ClassA::sumA1(int x,int y){
+	return x+y;
+}
+int ClassA::sumA2(int x,int y){
+	return x+y;
+}
---
+++ googletest/test3/src/ClassB.cpp
@@ -0,0 +1,8 @@
+#include "ClassB.h"
+
+int ClassB::subB1(int x,int y){
+	return x-y;
+}
+int ClassB::subB2(int x,int y){
+	return x-y;
+}
